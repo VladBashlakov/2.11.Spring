4be569bc798a90e9394be339f7ce23c5cfaa7b02
Vlad <VladBashlakov@gmail.com>
2022-01-14 13:38:01 +0300

    2.11 Spring(3)

--- src/main/java/com/example/_211spring/service/impl/BasketServiceImpl.java
+++ src/main/java/com/example/_211spring/service/impl/BasketServiceImpl.java
@@ -3,12 +3,11 @@ package com.example._211spring.service.impl;
 import com.example._211spring.configurator.Basket;
 import com.example._211spring.service.BasketService;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.annotation.SessionScope;
+
 
 import java.util.ArrayList;
 
 @Service
-@SessionScope
 public class BasketServiceImpl implements BasketService {
 
     private final Basket basket;
